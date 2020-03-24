f34eb9310667eb70f6ae6037afff56a7deddab5e
Gerardo E. Cruz-Ortiz <59618057+astrogeco@users.noreply.github.com>
2020-03-24 10:08:40 -0400

    Increase version to 6.7.11 and update ReadMe

--- fsw/cfe-core/src/inc/cfe_version.h
+++ fsw/cfe-core/src/inc/cfe_version.h
@@ -95,7 +95,7 @@
 */
 #define CFE_MAJOR_VERSION         6
 #define CFE_MINOR_VERSION         7
-#define CFE_REVISION              10
+#define CFE_REVISION              11
 
 
 #endif  /* _cfe_version_ */
