fc8a0206e94533d48ee66843b48db8141ab08dd3
astrogeco <59618057+astrogeco@users.noreply.github.com>
2020-08-19 09:42:17 -0400

    Increase version to 6.8.0-rc1+dev13

--- fsw/cfe-core/src/inc/cfe_version.h
+++ fsw/cfe-core/src/inc/cfe_version.h
@@ -24,19 +24,19 @@
 /*! @file cfe_version.h
  * @brief Purpose:
  *     Provide version identifiers for the cFE core.
- *  
+ *
  * target_config.h contains extended version information within it.
  * This information is generated automatically by the build using
  * git to determine the most recent tag and commit id.
- * 
+ *
  */
 #include <target_config.h>
 
 
 
 /* Development Build Macro Definitions */
-#define CFE_BUILD_NUMBER 292 /*!< Development Build: Number of commits since baseline */
-#define CFE_BUILD_BASELINE "v6.7.0" /*!< Development Build: git tag that is the base for the current development */
+#define CFE_BUILD_NUMBER 13 /*!< Development Build: Number of commits since baseline */
+#define CFE_BUILD_BASELINE "v6.8.0-rc1+dev" /*!< Development Build: git tag that is the base for the current development */
 
 /* Version Macro Definitions */
 
@@ -48,27 +48,27 @@
 #define CFE_STR_HELPER(x) #x /*!< @brief Helper function to concatenate strings from integer macros */
 #define CFE_STR(x)        CFE_STR_HELPER(x) /*!< @brief Helper function to concatenate strings from integer macros */
 
-/*! @brief Development Build Version Number. 
+/*! @brief Development Build Version Number.
  * @details Baseline git tag + Number of commits since baseline. @n
  * See @ref cfsversions for format differences between development and release versions.
  */
-#define CFE_SRC_VERSION CFE_BUILD_BASELINE "+dev" CFE_STR(CFE_BUILD_NUMBER) 
+#define CFE_SRC_VERSION CFE_BUILD_BASELINE "+dev" CFE_STR(CFE_BUILD_NUMBER)
 
 /*! @brief Development Build Version String.
  * @details Reports the current development build's baseline, number, and name. Also includes a note about the latest official version. @n
- * See @ref cfsversions for format differences between development and release versions. 
-*/          
+ * See @ref cfsversions for format differences between development and release versions.
+*/
 #define CFE_VERSION_STRING                                                          \
     " cFE DEVELOPMENT BUILD "                                                     \
     CFE_SRC_VERSION " (Codename: Bootes)"       /* Codename for current development */  \
     ", Last Official Release: cfe v6.7.0"   /* For full support please use this version */
 
 
-/*! @brief OSAL Version Definitions. 
+/*! @brief OSAL Version Definitions.
  * @details Allows for backwards compatibility. @n
- * This will be defined by osal in the future 
+ * This will be defined by osal in the future
  */
-#ifndef OS_VERSION 
+#ifndef OS_VERSION
 #define OS_VERSION                  \
     CFE_STR(OS_MAJOR_VERSION) "."   \
     CFE_STR(OS_MINOR_VERSION) "."   \
@@ -81,6 +81,6 @@
 "cFS Versions:"                      \
   " cfe "  CFE_SRC_VERSION                      \
   ", osal "  OS_VERSION                       \
-  ", psp "  /* CFE_PSP_VERSION is defined at runtime */ 
+  ", psp "  /* CFE_PSP_VERSION is defined at runtime */
 
 #endif /* _cfe_version_ */
