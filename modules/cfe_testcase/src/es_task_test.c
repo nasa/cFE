64ed1761020a7be41538cf8691a4bb5e1b612a50
Alex Campbell <alex.campbell@nasa.gov>
2021-04-09 11:27:05 -0400

    Base Cases

--- modules/cfe_testcase/src/es_task_test.c
+++ modules/cfe_testcase/src/es_task_test.c
@@ -33,16 +33,85 @@
 
 #include "cfe_test.h"
 
+uint32 count = 0;
+
+void TaskFunction(void)
+{
+    while (count < 200)
+    {
+        count += 1;
+        OS_TaskDelay(100);
+    }
+    return;
+}
+
+void TaskExitFunction(void)
+{
+    while (count < 200)
+    {
+        count += 1;
+        CFE_ES_ExitChildTask();
+    }
+    return;
+}
+
 void TestCreateChild(void)
 {
-    UtPrintf("Testing: CFE_ES_CreateChildTask");
+    UtPrintf("Testing: CFE_ES_CreateChildTask, CFE_ES_GetTaskIDByName, CFE_ES_GetTaskName, CFE_ES_DeleteChildTask");
 
-    UtAssert_NA("Inside Child Task");
+    CFE_ES_TaskId_t            TaskId;
+    const char *               TaskName = "CHILD_TASK_1";
+    CFE_ES_TaskId_t            TaskIdByName;
+    char                       TaskNameBuf[OS_MAX_API_NAME + 4];
+    CFE_ES_StackPointer_t      StackPointer = CFE_ES_TASK_STACK_ALLOCATE;
+    size_t                     StackSize    = CFE_PLATFORM_ES_PERF_CHILD_STACK_SIZE;
+    CFE_ES_TaskPriority_Atom_t Priority     = CFE_PLATFORM_ES_PERF_CHILD_PRIORITY;
+    uint32                     Flags        = 0;
+    int                        countCopy;
 
+    UtAssert_INT32_EQ(CFE_ES_CreateChildTask(&TaskId, TaskName, TaskFunction, StackPointer, StackSize, Priority, Flags),
+                      CFE_SUCCESS);
+
+    UtAssert_INT32_EQ(CFE_ES_GetTaskIDByName(&TaskIdByName, TaskName), CFE_SUCCESS);
+    UtAssert_ResourceID_EQ(TaskIdByName, TaskId);
+
+    UtAssert_INT32_EQ(CFE_ES_GetTaskName(TaskNameBuf, TaskId, sizeof(TaskNameBuf)), CFE_SUCCESS);
+    UtAssert_StrCmp(TaskNameBuf, TaskName, "CFE_ES_GetTaskName() = %s", TaskNameBuf);
+
+    OS_TaskDelay(500);
+
+    countCopy = count;
+
+    UtAssert_INT32_EQ(CFE_ES_DeleteChildTask(TaskId), CFE_SUCCESS);
+
+    OS_TaskDelay(500);
+
+    UtAssert_INT32_EQ(countCopy, count);
+}
+
+void TestExitChild(void)
+{
+    UtPrintf("Testing: CFE_ES_ExitChildTask");
+
+    CFE_ES_TaskId_t            TaskId;
+    const char *               TaskName     = "CHILD_TASK_1";
+    CFE_ES_StackPointer_t      StackPointer = CFE_ES_TASK_STACK_ALLOCATE;
+    size_t                     StackSize    = CFE_PLATFORM_ES_PERF_CHILD_STACK_SIZE;
+    CFE_ES_TaskPriority_Atom_t Priority     = CFE_PLATFORM_ES_PERF_CHILD_PRIORITY;
+    uint32                     Flags        = 0;
+    count                                   = 0;
+
+    UtAssert_INT32_EQ(
+        CFE_ES_CreateChildTask(&TaskId, TaskName, TaskExitFunction, StackPointer, StackSize, Priority, Flags),
+        CFE_SUCCESS);
+    OS_TaskDelay(500);
+    UtAssert_INT32_EQ(count, 1);
 }
 
 int32 ESTaskTestSetup(int32 LibId)
 {
-    UtTest_Add(TestCreateChild, NULL, NULL, "Test Create Child");   
+    UtTest_Add(TestCreateChild, NULL, NULL, "Test Create Child");
+    UtTest_Add(TestExitChild, NULL, NULL, "Test Exit Child");
+
     return CFE_SUCCESS;
 }
