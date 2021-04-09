f510be9ea15366f025be838dbf2137ad6b1522d5
Joseph Hickey <joseph.p.hickey@nasa.gov>
2021-04-09 14:56:56 -0400

    Fix #1281, functional test as app instead of library
    Merge the "testrunner" app and put the runner logic into cfe_assert lib.
    Converts the "testcase" library into an app that simply calls the logic
    in cfe_assert to do the test.
    
    This means that the test can be run on demand by issuing the ES
    start app command, rather than putting it into the startup script.

--- modules/cfe_assert/src/cfe_assert_runner.c
+++ modules/cfe_assert/src/cfe_assert_runner.c
@@ -31,8 +31,7 @@
 
 #include "cfe.h"
 
-#include "cfe_testrunner.h"
-#include "cfe_assert.h"
+#include "cfe_assert_priv.h"
 
 #include "uttest.h"
 #include "utbsp.h"
@@ -55,7 +54,7 @@
  *
  * The value is in milliseconds.
  */
-#define CFE_TESTRUNNER_START_DELAY 4000
+#define CFE_TESTRUNNER_START_DELAY 1000
 
 /*
  * This uses the message type as the event ID, because these are already
@@ -70,7 +69,7 @@ static CFE_EVS_BinFilter_t CFE_TR_EventFilters[] = {
     {UTASSERT_CASETYPE_DEBUG, CFE_EVS_NO_FILTER},
 };
 
-void CFE_TR_StatusReport(uint8 MessageType, const char *Prefix, const char *OutputMessage)
+void CFE_Assert_StatusReport(uint8 MessageType, const char *Prefix, const char *OutputMessage)
 {
     uint16 EventType;
 
@@ -101,68 +100,123 @@ void CFE_TR_StatusReport(uint8 MessageType, const char *Prefix, const char *Outp
     CFE_EVS_SendEvent(MessageType, EventType, "[%5s] %s", Prefix, OutputMessage);
 }
 
-/*
- * Entry point for this application
- */
-void CFE_TR_AppMain(void)
+int32 CFE_Assert_RegisterTest(const char *TestName)
 {
-    int32  rc;
-    uint32 RunStatus;
+    int32 rc;
+    char  SetupSegmentName[64];
 
     rc = CFE_EVS_Register(CFE_TR_EventFilters, sizeof(CFE_TR_EventFilters) / sizeof(CFE_EVS_BinFilter_t),
                           CFE_EVS_EventFilter_BINARY);
     if (rc != CFE_SUCCESS)
     {
         CFE_ES_WriteToSysLog("Error from CFE_EVS_Register: %08lx\n", (unsigned long)rc);
-        return;
+        return rc;
     }
 
     /*
      * Delay until the system reaches "operational" state -- this is when all libs have initialized
      * and all apps have reached their RunLoop.
+     *
+     * If already operational then this should return immediately.  This may be the case if/when
+     * test apps are started via ES command.
      */
     rc = CFE_ES_WaitForSystemState(CFE_ES_SystemState_OPERATIONAL, CFE_TESTRUNNER_MAX_STARTUP_WAIT);
     if (rc != CFE_SUCCESS)
     {
         CFE_ES_WriteToSysLog("Error while waiting for OPERATIONAL state: %08lx\n", (unsigned long)rc);
+        return rc;
+    }
+
+    /*
+     * Acquire the mutex.  This is needed because UtAssert and its data structures are not thread-safe.
+     * Only one test app should use UtAssert facilities at a given time.
+     */
+    rc = OS_MutSemTake(CFE_Assert_Global.AccessMutex);
+    if (rc != CFE_SUCCESS)
+    {
+        CFE_ES_WriteToSysLog("%s(): Error from OS_MutSemTake(): %d\n", __func__, (int)rc);
+        return CFE_STATUS_EXTERNAL_RESOURCE_FAIL;
+    }
+
+    /*
+     * After acquiring mutex, record the fact that this app now "owns" the assert functions
+     */
+    rc = CFE_ES_GetAppID(&CFE_Assert_Global.OwnerAppId);
+    if (rc != CFE_SUCCESS)
+    {
+        CFE_ES_WriteToSysLog("%s(): Error from CFE_ES_GetAppId(): %08x\n", __func__, (unsigned int)rc);
+        OS_MutSemGive(CFE_Assert_Global.AccessMutex);
+        return rc;
+    }
+
+    /*
+     * This means the system is operational and at least one app needs to run tests.
+     * Update library state accordingly.  The first test app that gets to this point
+     * will handle this.
+     */
+    if (CFE_Assert_Global.LibState != CFE_Assert_State_ACTIVE)
+    {
+        UtAssert_EndTest();
+        CFE_Assert_Global.LibState = CFE_Assert_State_ACTIVE;
+
+        OS_TaskDelay(CFE_TESTRUNNER_START_DELAY);
+    }
+
+    /*
+     * This resets the underlying UtAssert test list to a clean slate
+     *
+     * NOTE: this is not the ideal API here; UtAssert was originally designed to
+     * run one set of tests and then exit.  In this environment it might be used
+     * repeatedly by different apps.  The "EarlyInit" will wipe and reset the
+     * internal global, which works, but is heavy handed and doesn't account for
+     * the possibility that tests have been added without running.
+     */
+    UtTest_EarlyInit();
+
+    strncpy(CFE_Assert_Global.CurrentTestName, TestName, sizeof(CFE_Assert_Global.CurrentTestName) - 1);
+    CFE_Assert_Global.CurrentTestName[sizeof(CFE_Assert_Global.CurrentTestName) - 1] = 0;
+
+    /* Use the local status report function for the remainder of tests */
+    CFE_Assert_RegisterCallback(CFE_Assert_StatusReport);
+
+    /* Start a test group in case UtAssert is used during setup phase */
+    snprintf(SetupSegmentName, sizeof(SetupSegmentName), "%s TEST SETUP", TestName);
+    UtAssert_BeginTest(SetupSegmentName);
+
+    return CFE_SUCCESS;
+}
+
+void CFE_Assert_ExecuteTest(void)
+{
+    int32          rc;
+    CFE_ES_AppId_t AppId;
+
+    /*
+     * Sanity check - This should only be called from the same app after CFE_Assert_RegisterTest()
+     */
+    rc = CFE_ES_GetAppID(&AppId);
+    if (rc != CFE_SUCCESS || !CFE_RESOURCEID_TEST_EQUAL(AppId, CFE_Assert_Global.OwnerAppId))
+    {
+        CFE_ES_WriteToSysLog("%s(): Invalid calling context, CFE_ES_GetAppId() rc=%08x AppId=%lx, OwnerAppId=%lx\n",
+                             __func__, (unsigned int)rc, CFE_RESOURCEID_TO_ULONG(AppId),
+                             CFE_RESOURCEID_TO_ULONG(CFE_Assert_Global.OwnerAppId));
         return;
     }
 
-    /*
-     * Startup Phase has ended.
-     */
     UtAssert_EndTest();
 
-    /*
-     * Use the local status report function for the remainder of tests
-     */
-    CFE_Assert_RegisterCallback(CFE_TR_StatusReport);
+    OS_TaskDelay(CFE_TESTRUNNER_START_DELAY);
 
-    /*
-     * Note - in a normal app this would be a while loop,
-     * but is just an "if" right now as it only runs once.
-     *
-     * A future enhancement to this app might be to create an SB pipe
-     * and execute tests based on the receipt of a command to do so.
-     *
-     * For now, it just works in a one-shot mode to run tests that were
-     * registered during startup, then it self-exits.
-     */
-    RunStatus = CFE_ES_RunStatus_APP_RUN;
-    if (CFE_ES_RunLoop(&RunStatus))
+    /* Run all registered test cases. */
+    UtTest_Run();
+
+    /* unregister the callback and unset the appid */
+    CFE_Assert_RegisterCallback(NULL);
+
+    /* Release the access mutex so the next test program can run */
+    rc = OS_MutSemGive(CFE_Assert_Global.AccessMutex);
+    if (rc != CFE_SUCCESS)
     {
-        OS_TaskDelay(CFE_TESTRUNNER_START_DELAY);
-
-        /*
-         * Run all registered test cases.
-         */
-        UtTest_Run();
-
-        /*
-         * Exit the main task.
-         */
-        RunStatus = CFE_ES_RunStatus_APP_EXIT;
+        CFE_ES_WriteToSysLog("%s(): Error from OS_MutSemGive(): %d\n", __func__, (int)rc);
     }
-
-    CFE_ES_ExitApp(RunStatus);
 }
