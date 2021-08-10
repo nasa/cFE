3f2ee86460b5c89ad483774ad94d1d7fcff86e8e
Alex Campbell <alex.campbell@nasa.gov>
2021-08-10 10:29:20 -0400

    Fix #1649, Add comment for logic dependency

--- modules/cfe_testcase/src/evs_filters_test.c
+++ modules/cfe_testcase/src/evs_filters_test.c
@@ -37,6 +37,11 @@ void TestResetFilters(void)
 {
     UtPrintf("Testing: CFE_EVS_ResetFilter, CFE_EVS_ResetAllFilters");
 
+    /* Test logic below depends on the test case registering an EID of 1 and not registering 0, and the resets in theory
+     * could impact test behavior/management related to events. Although the expectation is either all or none of an EID
+     * would be filtered (no use case for a "counting" filter within the test app) so for normal use this is no impact.
+     */
+
     UtAssert_INT32_EQ(CFE_EVS_ResetFilter(1), CFE_SUCCESS);
     UtAssert_INT32_EQ(CFE_EVS_ResetAllFilters(), CFE_SUCCESS);
 
