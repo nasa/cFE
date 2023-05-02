ef4d3597320305bc4fcb56c64d508cbb1c8d1962
Joseph Hickey <joseph.p.hickey@nasa.gov>
2023-05-02 16:28:00 -0400

    Fix #2320, reorganize CORE_API header files
    Organize CORE_API header file contents according to the convention

--- modules/core_api/mission_build.cmake
+++ modules/core_api/mission_build.cmake
@@ -13,12 +13,19 @@
 generate_config_includefile(
     FILE_NAME           "cfe_mission_cfg.h"
     MATCH_SUFFIX        "mission_cfg.h"
+    FALLBACK_FILE        "${CMAKE_CURRENT_LIST_DIR}/config/default_cfe_mission_cfg.h"
     PREFIXES            ${MISSIONCONFIG} cfe
 )
 
 generate_config_includefile(
     FILE_NAME           "cfe_perfids.h"
     MATCH_SUFFIX        "perfids.h"
+    FALLBACK_FILE        "${CMAKE_CURRENT_LIST_DIR}/config/default_cfe_perfids.h"
     PREFIXES            ${MISSIONCONFIG} cfe
 )
 
+generate_config_includefile(
+    FILE_NAME           "cfe_core_api_interface_cfg.h"
+    FALLBACK_FILE        "${CMAKE_CURRENT_LIST_DIR}/config/default_cfe_core_api_interface_cfg.h"
+    PREFIXES            ${BUILD_CONFIG}
+)
