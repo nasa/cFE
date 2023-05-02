f0edf1f23e3cea59c21b11296b8aea83528dcf1a
Joseph Hickey <joseph.p.hickey@nasa.gov>
2023-05-02 16:28:00 -0400

    Fix #2320, reorganize CORE_PRIVATE header files
    Organize CORE_PRIVATE header file contents according to the convention

---
+++ modules/core_private/config/default_cfe_platform_cfg.h
@@ -0,0 +1,44 @@
+/************************************************************************
+ * NASA Docket No. GSC-18,719-1, and identified as “core Flight System: Bootes”
+ *
+ * Copyright (c) 2020 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ************************************************************************/
+
+/**
+ * @file
+ *
+ * Purpose:
+ *   This header file contains the platform configuration parameters.
+ *
+ * Notes:
+ *   The impact of changing these configurations from their default value is
+ *   not yet documented.  Changing these values may impact the performance
+ *   and functionality of the system.
+ *
+ * Author:   R.McGraw/SSI
+ *
+ */
+
+#ifndef CFE_PLATFORM_CFG_H
+#define CFE_PLATFORM_CFG_H
+
+#include "cfe_core_private_internal_cfg.h"
+#include "cfe_es_platform_cfg.h"
+#include "cfe_evs_platform_cfg.h"
+#include "cfe_sb_platform_cfg.h"
+#include "cfe_tbl_platform_cfg.h"
+#include "cfe_time_platform_cfg.h"
+
+#endif /* CFE_PLATFORM_CFG_H */
